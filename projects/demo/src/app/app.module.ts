b9d097d6394f49ba1d60a9308749a889eacc62fa
mcgear <michael.gearhardt@gmail.com>
2020-04-01 01:24:02 -0600

    Interesting changes

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FathymSharedModule, MaterialModule } from '@lcu/common';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './controls/home/home.component';
-import { DocumentationComponent } from './controls/documentation/documentation.component';
-import { LcuDocumentationModule } from '@lowcodeunit/lcu-documentation-common';
-import { LcuLimitedTrialModule } from '@napkin-ide/lcu-limited-trial-common';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FathymSharedModule, MaterialModule, LCUServiceSettings } from "@lcu/common";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from "./controls/home/home.component";
+import { DocumentationComponent } from "./controls/documentation/documentation.component";
+import { LcuDocumentationModule } from "@lowcodeunit/lcu-documentation-common";
+import { LcuLimitedTrialModule } from "@napkin-ide/lcu-limited-trial-common";
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    DocumentationComponent
-  ],
+  declarations: [AppComponent, HomeComponent, DocumentationComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
@@ -23,11 +20,16 @@ import { LcuLimitedTrialModule } from '@napkin-ide/lcu-limited-trial-common';
     FathymSharedModule,
     MaterialModule,
     FlexLayoutModule,
-    LcuDocumentationModule.forRoot(),
+    // LcuDocumentationModule.forRoot(),
     LcuLimitedTrialModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LCUServiceSettings,
+      useValue: FathymSharedModule.DefaultServiceSettings(environment)
+    }
+  ],
   bootstrap: [AppComponent],
   exports: [LcuLimitedTrialModule]
 })
-export class AppModule { }
+export class AppModule {}
