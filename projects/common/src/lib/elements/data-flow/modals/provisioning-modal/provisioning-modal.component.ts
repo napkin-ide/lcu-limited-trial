d609bdd46097ed28276e59a22401cf5132a3a75f
Robert Wise <robert.wise@fathym.com>
2020-04-08 12:41:51 -0600

    Added new Provisioning modal and updated lcu-data-flow package to get latest changes

---
+++ projects/common/src/lib/elements/data-flow/modals/provisioning-modal/provisioning-modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'lcu-provisioning-modal',
+  templateUrl: './provisioning-modal.component.html',
+  styleUrls: ['./provisioning-modal.component.scss']
+})
+export class ProvisioningModalComponent {
+
+  constructor(
+    private dialogRef: MatDialogRef<ProvisioningModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public Data: any
+  ) { }
+
+  public onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  public BuyNow(): void {
+    window.location.href = 'https://fathym.com/low-code-framework/';
+  }
+}
--- projects/common/src/lib/lcu-limited-trial.module.ts
+++ projects/common/src/lib/lcu-limited-trial.module.ts
@@ -10,7 +10,7 @@ import { LimitedDataAppsManagementStateContext } from './state/data-apps/limited
 import { LimitedDataFlowManagementStateContext } from './state/data-flow/limited-data-flow-management-state.context';
 import { ListItemComponent } from './controls/list-item/list-item.component';
 import { LcuDataFlowModule } from '@napkin-ide/lcu-data-flow-common';
-import { ConfirmationModalComponent } from './elements/data-flow/modals/confirmation-modal/confirmation-modal.component';
+import { ProvisioningModalComponent } from './elements/data-flow/modals/provisioning-modal/provisioning-modal.component';
 import { DataAppsConfigComponent } from './controls/data-apps/data-apps-config/data-apps-config.component';
 
 @NgModule({
@@ -19,7 +19,7 @@ import { DataAppsConfigComponent } from './controls/data-apps/data-apps-config/d
     LcuLimitedTrialDataAppsElementComponent,
     LcuLimitedTrialDataFlowElementComponent,
     ListItemComponent,
-    ConfirmationModalComponent,
+    ProvisioningModalComponent,
     AppListComponent,
     DataAppsConfigComponent
   ],
@@ -44,7 +44,7 @@ import { DataAppsConfigComponent } from './controls/data-apps/data-apps-config/d
     LcuLimitedTrialDataAppsElementComponent,
     LcuLimitedTrialDataFlowElementComponent,
     ListItemComponent,
-    ConfirmationModalComponent,
+    ProvisioningModalComponent,
     AppListComponent,
     DataAppsConfigComponent
   ]
