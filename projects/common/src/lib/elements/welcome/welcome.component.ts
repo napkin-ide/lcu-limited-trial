42caf49a9246147ba370d4b4256f30798861c0bf
William Horst <william.horst@fathym.com>
2020-04-10 11:54:03 -0600

    checkpoint - ui updated with actual mock data and accordion working properly

--- projects/common/src/lib/elements/welcome/welcome.component.ts
+++ projects/common/src/lib/elements/welcome/welcome.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit, Injector } from '@angular/core';
 import { LCUElementContext, LcuElementComponent } from '@lcu/common';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 export class LcuLimitedTrialWelcomeElementState { }
 
 export class LcuLimitedTrialWelcomeContext extends LCUElementContext<LcuLimitedTrialWelcomeElementState> { }
 
 export const SELECTOR_LCU_LIMITED_TRIAL_WELCOME_ELEMENT = 'lcu-limited-trial-welcome-element';
-
-import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: SELECTOR_LCU_LIMITED_TRIAL_WELCOME_ELEMENT,
@@ -19,15 +18,19 @@ export class LcuLimitedTrialWelcomeElementComponent extends LcuElementComponent<
   //  Fields
 
   //  Properties
-  public PanelOpenState = false;
+  public DividedJourneys: Array<{ journeyName: string, journeys: Array<any> }>;
   public Journeys: Array<any>;
+  public PanelOpenState: boolean;
 
   public get UserRoles() { return Object.keys(UserRole); }
 
   //  Constructors
   constructor(protected injector: Injector) {
     super(injector);
+    this.PanelOpenState = false;
+    this.DividedJourneys = [];
     this.Journeys = this.getJourneys();
+    this.divideJourneys();
   }
 
   //  Life Cycle
@@ -38,6 +41,16 @@ export class LcuLimitedTrialWelcomeElementComponent extends LcuElementComponent<
   //  API Methods
 
   //  Helpers
+  protected divideJourneys() {
+    this.UserRoles.forEach(role => {
+      this.DividedJourneys.push({ journeyName: role, journeys: [] });
+    });
+    this.Journeys.forEach(journey => {
+      journey.roles.forEach((role: any) => {
+        this.DividedJourneys.find(j => j.journeyName === role).journeys.push(journey);
+      });
+    });
+  }
   protected getJourneys(): Array<any> {
     return [
       {
@@ -45,56 +58,64 @@ export class LcuLimitedTrialWelcomeElementComponent extends LcuElementComponent<
         videoUrl: 'https://player.vimeo.com/video/403508452',
         uses: ['Devices', 'Data Flow', 'Data Science'],
         description: 'Build and connect edge devices, securely manage, visualize and analyze your data, and take action on your intelligence.',
-        roles: [UserRole.Developer]
+        roles: [UserRole.Developer],
+        active: true
       },
       {
-        name: 'Cloud Orchestration',
+        name: 'Application Development',
         videoUrl: 'https://player.vimeo.com/video/403508452',
-        uses: ['AI/ML', 'Analytics', 'Reporting'],
-        description: 'Develop data applications from existing and new enterprise data. Leverage existing tools with new at a rapid pace.',
-        roles: [UserRole.Developer]
+        uses: ['JS Apps', 'Security', 'Dev Tools'],
+        description: 'Develop JavaScript applications in the framework of your choosing and easily deploy, secure, and manage at scale.',
+        roles: [UserRole.Developer],
+        active: false
       },
       {
-        name: 'Cloud Orchestration',
+        name: 'Cloud Development',
         videoUrl: 'https://player.vimeo.com/video/403508452',
-        uses: ['AI/ML', 'Analytics', 'Reporting'],
-        description: 'Develop data applications from existing and new enterprise data. Leverage existing tools with new at a rapid pace.',
-        roles: [UserRole.Developer]
-      },
-      {
-        name: 'Cloud Orchestration',
-        videoUrl: 'https://player.vimeo.com/video/403508452',
-        uses: ['AI/ML', 'Analytics', 'Reporting'],
-        description: 'Develop data applications from existing and new enterprise data. Leverage existing tools with new at a rapid pace.',
-        roles: [UserRole.Developer]
-      },
-      {
-        name: 'Cloud Orchestration',
-        videoUrl: 'https://player.vimeo.com/video/403508452',
-        uses: ['AI/ML', 'Analytics', 'Reporting'],
-        description: 'Develop data applications from existing and new enterprise data. Leverage existing tools with new at a rapid pace.',
-        roles: [UserRole.Developer]
-      },
-      {
-        name: 'Cloud Orchestration',
-        videoUrl: 'https://player.vimeo.com/video/403508452',
-        uses: ['AI/ML', 'Analytics', 'Reporting'],
-        description: 'Develop data applications from existing and new enterprise data. Leverage existing tools with new at a rapid pace.',
-        roles: [UserRole.Developer]
-      },
-      {
-        name: 'Enterprise Intranets',
-        videoUrl: 'https://player.vimeo.com/video/403508452',
-        uses: ['AI/ML', 'Analytics', 'Reporting'],
-        description: 'Develop data applications from existing and new enterprise data. Leverage existing tools with new at a rapid pace.',
-        roles: [UserRole.Designer]
+        uses: ['DevOps', 'IaC', 'Data Flow'],
+        description: 'Rapidly set up and manage enterprise grade, best practice cloud infrastructures and leverage them to build apps and APIs.',
+        roles: [UserRole.Developer],
+        active: false
       },
       {
         name: 'Data Development',
         videoUrl: 'https://player.vimeo.com/video/403508452',
         uses: ['AI/ML', 'Analytics', 'Reporting'],
         description: 'Develop data applications from existing and new enterprise data. Leverage existing tools with new at a rapid pace.',
-        roles: [UserRole.Administrator]
+        roles: [UserRole.Developer],
+        active: false
+      },
+      {
+        name: 'Cloud Orchestration',
+        videoUrl: 'https://player.vimeo.com/video/403508452',
+        uses: ['DevOps', 'IaC', 'Data Flow'],
+        description: 'Rapidly set up and manage enterprise grade, best practice cloud infrastructures and leverage them to build apps and APIs.',
+        roles: [UserRole.Developer],
+        active: false
+      },
+      {
+        name: 'Enterprise Intranets',
+        videoUrl: 'https://player.vimeo.com/video/403508452',
+        uses: ['Dashboards', 'Reporting', 'Identity'],
+        description: 'Leverage our Enterprise IDE to rapidly pull together open source and custom LCUs to drive value in your organization.',
+        roles: [UserRole.Developer],
+        active: false
+      },
+      {
+        name: 'Designer Tools',
+        videoUrl: 'https://player.vimeo.com/video/403508452',
+        uses: ['AI/ML', 'Analytics', 'Reporting'],
+        description: 'Develop data applications from existing and new enterprise data. Leverage existing tools with new at a rapid pace.',
+        roles: [UserRole.Designer],
+        active: false
+      },
+      {
+        name: 'Admin Tools',
+        videoUrl: 'https://player.vimeo.com/video/403508452',
+        uses: ['AI/ML', 'Analytics', 'Reporting'],
+        description: 'Develop data applications from existing and new enterprise data. Leverage existing tools with new at a rapid pace.',
+        roles: [UserRole.Administrator],
+        active: false
       }
     ];
   }
