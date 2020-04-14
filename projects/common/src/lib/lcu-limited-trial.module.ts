bab5b151525cad434f7945515469d6ee19fc6239
xchanin <me@shannonbruns.com>
2020-04-14 12:37:13 -0600

    trying to fix NPM build issue

--- projects/common/src/lib/lcu-limited-trial.module.ts
+++ projects/common/src/lib/lcu-limited-trial.module.ts
@@ -1,25 +1,25 @@
+import { LcuDataFlowModule } from '@napkin-ide/lcu-data-flow-common';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FathymSharedModule, MaterialModule } from '@lcu/common';
-import { LcuLimitedTrialWelcomeElementComponent, SafePipe } from './elements/welcome/welcome.component';
-import { LcuLimitedTrialDataAppsElementComponent } from './elements/data-apps/data-apps.component';
-import { LcuLimitedTrialDataFlowElementComponent } from './elements/data-flow/data-flow.component';
 import { AppListComponent } from './controls/data-apps/app-list/app-list.component';
 import { LimitedDataAppsManagementStateContext } from './state/data-apps/limited-data-apps-management-state.context';
 import { LimitedDataFlowManagementStateContext } from './state/data-flow/limited-data-flow-management-state.context';
-import { ListItemComponent } from './controls/list-item/list-item.component';
-import { LcuDataFlowModule } from '@napkin-ide/lcu-data-flow-common';
+// import { ListItemComponent } from './controls/list-item/list-item.component';
+
 import { AddAppComponent } from './elements/data-apps/add-app/add-app.component';
 import { ProvisioningModalComponent } from './elements/data-flow/modals/provisioning-modal/provisioning-modal.component';
 import { DataAppsConfigComponent } from './elements/data-apps/data-apps-config/data-apps-config.component';
+import { LcuLimitedTrialWelcomeElementComponent, SafePipe } from './elements/welcome/welcome.component';
+import { LcuLimitedTrialDataAppsElementComponent } from './elements/data-apps/data-apps.component';
+import { LcuLimitedTrialDataFlowElementComponent } from './elements/data-flow/data-flow.component';
 
 @NgModule({
   declarations: [
     LcuLimitedTrialWelcomeElementComponent,
     LcuLimitedTrialDataAppsElementComponent,
     LcuLimitedTrialDataFlowElementComponent,
-    ListItemComponent,
     ProvisioningModalComponent,
     DataAppsConfigComponent,
     AppListComponent,
@@ -32,26 +32,24 @@ import { DataAppsConfigComponent } from './elements/data-apps/data-apps-config/d
     ReactiveFormsModule,
     FlexLayoutModule,
     MaterialModule,
-    LcuDataFlowModule.forRoot()
+   LcuDataFlowModule.forRoot()
   ],
   exports: [
     LcuLimitedTrialWelcomeElementComponent,
     LcuLimitedTrialDataAppsElementComponent,
     LcuLimitedTrialDataFlowElementComponent,
-    ListItemComponent,
     DataAppsConfigComponent,
     AppListComponent,
-    AddAppComponent // need to remove this one and replace with the ListItemComponent
+    AddAppComponent
   ],
   entryComponents: [
     LcuLimitedTrialWelcomeElementComponent,
     LcuLimitedTrialDataAppsElementComponent,
     LcuLimitedTrialDataFlowElementComponent,
-    ListItemComponent,
     ProvisioningModalComponent,
     DataAppsConfigComponent,
     AppListComponent,
-    AddAppComponent // need to remove this one and replace with the ListItemComponent
+    AddAppComponent
   ]
 })
 export class LcuLimitedTrialModule {
