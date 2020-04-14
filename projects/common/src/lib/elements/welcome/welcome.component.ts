f4e6ddab4a449d1b1a361541aac3aaf5558e4b70
mcgear <michael.gearhardt@gmail.com>
2020-04-13 18:53:53 -0600

    Gotta find new npm service

--- projects/common/src/lib/elements/welcome/welcome.component.ts
+++ projects/common/src/lib/elements/welcome/welcome.component.ts
@@ -2,29 +2,55 @@ import { Component, OnInit, Injector } from '@angular/core';
 import { LCUElementContext, LcuElementComponent } from '@lcu/common';
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { JourneyRoleTypes } from '../../state/journeys/journeys.state';
+import {
+  JourneyRoleTypes,
+  LimitedJourneysManagementState,
+  JourneyOption,
+  JourneyContentTypes,
+} from '../../state/journeys/journeys.state';
+import { LimitedJourneysManagementStateContext } from '../../state/journeys/journeys-state.context';
 
-export class LcuLimitedTrialWelcomeElementState { }
+export class LcuLimitedTrialWelcomeElementState {}
 
-export class LcuLimitedTrialWelcomeContext extends LCUElementContext<LcuLimitedTrialWelcomeElementState> { }
+export class LcuLimitedTrialWelcomeContext extends LCUElementContext<
+  LcuLimitedTrialWelcomeElementState
+> {}
 
-export const SELECTOR_LCU_LIMITED_TRIAL_WELCOME_ELEMENT = 'lcu-limited-trial-welcome-element';
+export const SELECTOR_LCU_LIMITED_TRIAL_WELCOME_ELEMENT =
+  'lcu-limited-trial-welcome-element';
 
 @Component({
   selector: SELECTOR_LCU_LIMITED_TRIAL_WELCOME_ELEMENT,
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
-export class LcuLimitedTrialWelcomeElementComponent extends LcuElementComponent<LcuLimitedTrialWelcomeContext> implements OnInit {
+export class LcuLimitedTrialWelcomeElementComponent
+  extends LcuElementComponent<LcuLimitedTrialWelcomeContext>
+  implements OnInit {
   //  Fields
 
   //  Properties
+  /**
+   * Content Types
+   */
+  public ContentTypes = JourneyContentTypes;
+
   public PanelOpenState: boolean;
 
-  public get UserRoles() { return Object.keys(JourneyRoleTypes); }
+  /**
+   * Current state
+   */
+  public State: LimitedJourneysManagementState;
+
+  public get JourneyRoles() {
+    return Object.keys(JourneyRoleTypes);
+  }
 
   //  Constructors
-  constructor(protected injector: Injector) {
+  constructor(
+    protected injector: Injector,
+    protected state: LimitedJourneysManagementStateContext
+  ) {
     super(injector);
     this.PanelOpenState = false;
   }
@@ -32,21 +58,33 @@ export class LcuLimitedTrialWelcomeElementComponent extends LcuElementComponent<
   //  Life Cycle
   public ngOnInit() {
     super.ngOnInit();
+
+    this.state.Context.subscribe((state: any) => {
+      this.State = state;
+
+      this.handleStateChanges();
+    });
   }
 
   //  API Methods
+  public ContainsRoleType(journey: JourneyOption, roleType: JourneyRoleTypes) {
+    return !!journey.Roles.find((r) => r === roleType);
+  }
 
   //  Helpers
+
+  /**
+   * Handle when the state is returned
+   */
+  protected handleStateChanges(): void {}
 }
 
 @Pipe({
-  name: 'safe'
+  name: 'safe',
 })
 export class SafePipe implements PipeTransform {
-
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer) {}
   transform(url: string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
-
 }
